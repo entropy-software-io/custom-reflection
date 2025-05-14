8749263cc41b736e833e20606893833f159531dd
Jon Christen <jon@entropysoftware.io>
2025-05-14 11:57:00 -0700

    Basic project setup

---
+++ Src/Main.cpp
@@ -0,0 +1,10 @@
+// Copyright (c) Entropy Software LLC
+// This file is licensed under the MIT License.
+// See the LICENSE file in the project root for more information.
+
+#include "Entropy/Reflection.h"
+
+int main(int argc, char* argv[])
+{
+
+}
